2e0ae166ac1ff0d8eb88b653c941d0647d2bf34b
Jesus Hernandez <jesusbeckan@gmail.com>
2023-01-26 13:57:06 -0500

    Adds compare functionality

--- src/index.js
+++ src/index.js
@@ -3,43 +3,59 @@ import { createStore } from './store/withImmer.js'
 
 const $text = document.querySelector('.js-count')
 const $button = document.querySelector('.js-increment')
-const $button2 = document.querySelector('.js-increment-2')
+const $restoreOne = document.querySelector('.js-restore-one')
+const $restoreTwo = document.querySelector('.js-restore-two')
+const lodash = window._
 
-const store = createStore({ count: 0, text: '', user: { name: 'Jesus Hernandez', range: [0, 1] } })
+const compare = (prevState, newState) => {
+  return lodash.isEqual(prevState, newState)
+}
+
+const store = createStore({
+  count: 0,
+  text: '',
+  user: {
+    name: 'Jesus Hernandez',
+    range: [0, 1] }
+  }, {
+    compare
+  }
+)
 
 window.GLOBAL_STORE = store
 
 window.addEventListener('DOMContentLoaded', () => {
-  store.subscribe(() => {
-    console.log('Global state', store.getState())
+  store.subscribe((state, prevState) => {
+    console.log('Global new state', { state, prevState })
   })
 
   store.subscribe(() => {
+    console.log('Updated count state', store.getState())
     $text.textContent = store.getState().count
   }, (state) => state.count)
 
   store.subscribe(() => {
-    console.log('Updated text state', store.getState())
+    // console.log('Updated text state', store.getState())
   }, (state) => state.text)
-
 
   $button.addEventListener('click', () => {
     store.setState((state) => {
       return {
         ...state,
         count: state.count + 1,
-        text: 'Typing',
-        user: {
-          name: 'Jesus',
-          lastName: 'Hernandez',
-          range: [1, 1]
-        }
       }
     })
   })
 
-  $button2.addEventListener('click', () => {
-    // Review when the state changes completely to Array
-    store.setState((state) => [])
+  $restoreOne.addEventListener('click', () => {
+    store.setState(() => {
+      return {}
+    })
+  })
+
+  $restoreTwo.addEventListener('click', () => {
+    store.setState((state) => {
+      return {}
+    }, true)
   })
 })
--- src/store/withImmer.js
+++ src/store/withImmer.js
@@ -1,30 +1,49 @@
 const { produce } = window.immer
+const lodash = window._
 
-export const createStore = (initialState = {}) => {
+export const createStore = (initialState = {}, options = {}) => {
   const listeners = new Set()
   const _inmutableState = produce((draft) => ({ ...initialState, ...draft }))
   let state = _inmutableState(initialState)
 
   const getState = () => state
 
-  const setState = (fn) => {
-    state = _inmutableState(fn({ ...state }))
+  const setState = (fn, force) => {
+    const prevState = state
+    const newState = fn(state)
+    let equals = false
 
-    listeners.forEach((listener) => {
-      if (listener.LISTENER) {
-        const currentState = listener.STATE
-        const nextState = listener.SELECTOR(state)
+    if (force) {
+      state = _inmutableState(newState)
+      initialState = {}
+    }
 
-        if (currentState !== nextState ) {
-          listener.STATE = nextState
-          listener()
+    if (typeof options?.compare === 'function') {
+      equals = options.compare(prevState, newState)
+    }
+
+    if (!equals) {
+      state = _inmutableState(newState)
+
+      listeners.forEach((listener) => {
+        const _prevState = lodash.cloneDeep(prevState)
+        const newState = lodash.cloneDeep(state)
+
+        if (listener.LISTENER) {
+          const currentState = listener.STATE
+          const nextState = listener.SELECTOR(state)
+
+          if (!Object.is(currentState, nextState)) {
+            listener.STATE = nextState
+            listener(newState, _prevState)
+          }
+
+          return
         }
 
-        return
-      }
-
-      listener()
-    })
+        listener(newState, _prevState)
+      })
+    }
   }
 
   const subscribe = (listener, selector) => {
